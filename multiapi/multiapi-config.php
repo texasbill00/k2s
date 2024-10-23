8930091c6288387e5621ae21b598d8d7221d7440
texasbill00 <texasbill00@proton.me>
2024-10-23 10:42:06 +0200

    Update multiapi-config.php

--- multiapi/multiapi-config.php
+++ multiapi/multiapi-config.php
@@ -1,7 +1,7 @@
 <?php
 
-	$pattern = '';
-	$url = '';
-	$phantomcookies = '';
+	$pattern = '/(?<host>k2s\.cc|keep2share\.cc|keep2s\.cc|fileboom\.me|fboom\.me|tezfiles\.com?)\/file\/(?<ufid>[\da-zA-Z\-]+)/i';
+	$url = 'https://api.{$host}/v1/files/{$ufid}';
+	$phantomcookies = 'https://' . $_SERVER['HTTP_HOST'] . '/phantomcookies/index.php';
 
 ?>
